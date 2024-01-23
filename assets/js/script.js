c4469db21d21e160f3a8b092e7d2fa75251db7b8
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-23 22:07:02 +0000

    created time block for each hour of the day from 9am - 5pm, just need to add these as rows to the container

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,26 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 $("#currentDay").text(dayjs().format('ddd, Do MMM YYYY, HH:mm'));
 
 // Create a time block for each hour of the day between 9am and 5pm
+for (let i = 9; i <= 17; i++) {
+    var timeBlocks = dayjs().hour(i).format('h A');
+    console.log(timeBlocks);
+
+    // Create a time block for each hour of the day
+
+    
+    // Add the time block to the container
+
+
+}
 
 
 // Add the time block to the container
+
+
+// Define how the app understands time
+// ie time in the past = time block greyed out
+// ie time in the future = time block in color
+
 
 
 // Add the event listener to the time block
@@ -19,4 +36,3 @@ $("#currentDay").text(dayjs().format('ddd, Do MMM YYYY, HH:mm'));
 
 
 // Save and load events for each time block
-

53fc4613f6a9821e1c3be0b6964f35d52330ccc3
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-23 10:20:46 +0000

    added rough pseudocode for the js file

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Pseudocode for the application
+// 1. Get the current day and time
+
+
+// 2. Add the current day and time to the header
+
+
+// 3. Create a time block for each hour of the day between 9am and 5pm
+
+
+// 4. Add the time block to the container
+
+
+// 5. Add the event listener to the time block
+
+
+// 6. Add the event listener to the save button
+
+
+// 7. Add the event listener to the clear button
+
+
+// 8. Save and load events for each time block
+

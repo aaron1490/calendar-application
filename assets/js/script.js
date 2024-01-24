132f9e6b344269acfa79214411347ba40d969262
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-24 12:12:34 +0000

    declared some variables for start and end time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,26 @@
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 $("#currentDay").text(dayjs().format('ddd, Do MMM YYYY, HH:mm'));
 
+// declare variables for start and end of day
+var start = 9;
+var end = 17;
+
+// Define how the app understands time
+var currentHour = dayjs().hour();
+console.log(currentHour);
+
+// ie time in the past = time block greyed out
+// var pastTime = currentHour > ;
+
+// ie time in the future = time block in color
+
+
 // Create a time block for each hour of the day between 9am and 5pm
-for (let i = 9; i <= 17; i++) {
-    var timeBlocks = dayjs().hour(i).format('h A');
+for (let i = start; i <= end; i++) {
+    var timeBlocks = dayjs().hour(i).format('HH A');
     console.log(timeBlocks);
 
-    // Create a time block for each hour of the day
-
-    
-    // Add the time block to the container
+    // Append the time block to the container    
 
 
 }
@@ -20,9 +31,6 @@ for (let i = 9; i <= 17; i++) {
 // Add the time block to the container
 
 
-// Define how the app understands time
-// ie time in the past = time block greyed out
-// ie time in the future = time block in color
 
 
 

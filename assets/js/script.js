beed17b00492365173ed38ee8da2d925f23b715f
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-25 11:09:06 +0000

    created the timeblocks and have appended them to the container within the HTML. Managed to get the times to show up, the text area to show up and the font awesome buttons to show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Pseudocode for the application
 // Get the current day and time and display in the header
 dayjs.extend(window.dayjs_plugin_advancedFormat);
-$("#currentDay").text(dayjs().format('ddd, Do MMM YYYY, HH:mm'));
+$("#currentDay").text(dayjs().format("ddd, Do MMM YYYY, HH:mm"));
 
 // declare variables for start and end of day
 var start = 9;
@@ -16,31 +16,25 @@ console.log(currentHour);
 
 // ie time in the future = time block in color
 
-
 // Create a time block for each hour of the day between 9am and 5pm
 for (let i = start; i <= end; i++) {
-    var timeBlocks = dayjs().hour(i).format('HH A');
-    console.log(timeBlocks);
+  var timeBlocks = dayjs().hour(i).format("HA");
+  console.log(timeBlocks);
 
-    // Append the time block to the container    
-
-
+  // Append the time block to the container
+  $(".container").append(`
+<div class="row time-block">
+    <div class="col-1 hour center">${timeBlocks}</div>
+    <textarea class="col-10 description"></textarea>
+    <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
+</div>
+`);
 }
-
-
-// Add the time block to the container
-
-
-
-
 
 // Add the event listener to the time block
 
-
 // Add the event listener to the save button
 
-
 // Add the event listener to the clear button
-
 
 // Save and load events for each time block

55383ee40887727bd6a2a6018a4ccd75c3e5b05d
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-26 20:28:49 +0000

    added function that checks the hour user is currenlty on and added this to the container div class as a css color styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,34 @@ $("#currentDay").text(dayjs().format("ddd, Do MMM YYYY, HH:mm"));
 var start = 9;
 var end = 17;
 
-// Define how the app understands time
 var currentHour = dayjs().hour();
 console.log(currentHour);
 
-// ie time in the past = time block greyed out
-// var pastTime = currentHour > ;
-
-// ie time in the future = time block in color
+// Define how the app understands time
+function whatHour(hour) {
+  // ie time in the past = time block greyed out
+  if (hour < currentHour) {
+    return "past";
+    // ie time in the present = time block in color
+  } else if (hour === currentHour) {
+    return "present";
+    // ie time in the future = time block in color
+  } else {
+    return "future";
+  }
+}
 
 // Create a time block for each hour of the day between 9am and 5pm
 for (let i = start; i <= end; i++) {
   var timeBlocks = dayjs().hour(i).format("HA");
   console.log(timeBlocks);
+  var timeStatus = whatHour(i);
 
   // Append the time block to the container
   $('.container').append(`
 <div class="row time-block">
     <div class="col-1 hour center">${timeBlocks}</div>
-    <textarea class="col-10 description"></textarea>
+    <textarea class="col-10 description ${timeStatus}"></textarea>
     <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
 </div>
 `);

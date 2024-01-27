6b804a76c743e7c83987152abe27265c9f5b4b69
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-27 20:42:08 +0000

    bug: fixed the bug that was causing issue with data not loading correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,18 @@ $("#currentDay").text(dayjs().format("ddd, Do MMM YYYY, HH:mm"));
 var start = 9;
 var end = 17;
 
+// get the current hour
 var currentHour = dayjs().hour();
-console.log(currentHour);
 
-// Define how the app understands time
+// Define how the app defines time
 function whatHour(hour) {
-  // ie time in the past = time block greyed out
+  // time in the past = time block greyed out
   if (hour < currentHour) {
     return "past";
-    // ie time in the present = time block in color
+    // time in the present = time block in red
   } else if (hour === currentHour) {
     return "present";
-    // ie time in the future = time block in color
+    // everything else is time in the future = time block in green
   } else {
     return "future";
   }
@@ -26,34 +26,36 @@ function whatHour(hour) {
 
 // Create a time block for each hour of the day between 9am and 5pm
 for (let i = start; i <= end; i++) {
+  // create a variable for the time format
   var timeBlocks = dayjs().hour(i).format("HA");
-  console.log(timeBlocks);
-  var timeStatus = whatHour(i);
-  $(`.${timeStatus}`).val(textValue);
 
+  // variable that runs the whatHour function to determines the status of the current hour
+  var timeStatus = whatHour(i);
 
   // Append the time block to the container
   $(".container").append(`
-<div class="row time-block">
-    <div class="col-2 col-sm-1 hour center">${timeBlocks}</div>
-    <textarea class="col-8 col-sm-10 description ${timeStatus}"></textarea>
-    <button class="col-2 col-sm-1 saveBtn saveButton"><i class="fas fa-save"></i></button>
-</div>
-`);
+    <div class="time-block row" data-time="${i}">
+      <div class="col-2 col-sm-1 hour center">${timeBlocks}</div>
+      <textarea class="col-8 col-sm-10 description ${timeStatus}"></textarea>
+      <button class="col-2 col-sm-1 saveBtn saveButton"><i class="fas fa-save"></i></button>
+    </div>
+  `);
+
+  // Load events from local storage for the current time block
+  var textValue = localStorage.getItem(`time-${i}`);
+  // if there is a saved value, set it in the textarea
+  if (textValue) {
+    $(`.time-block[data-time="${i}"] textarea`).val(textValue);
+  }
 }
 
 // Add the event listener to the time block save button
 $(".saveButton").on("click", function () {
   // Get the value of the textarea
   var textValue = $(this).siblings(".description").val();
+  // Get the data-time attribute of the parent time block
+  var time = $(this).parent().data("time");
 
-  // Save the value to local storage or perform any other action
-  localStorage.setItem($(this).siblings(".hour").text(), textValue);
+  // Save the value to local storage for the current time block
+  localStorage.setItem(`time-${time}`, textValue);
 });
-
-// Load events from local storage
-for (let i = start; i <= end; i++) {
-  var timeBlocks = dayjs().hour(i).format("HA");
-  var textValue = localStorage.getItem(timeBlocks);
-  $(`.${whatHour(i)}`).val(textValue);
-}
